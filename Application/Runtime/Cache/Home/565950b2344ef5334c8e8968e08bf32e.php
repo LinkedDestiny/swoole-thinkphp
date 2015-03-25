ad1850ba017cdec63df0e0f81660126dc066b495
lyaohe <893869272@qq.com>
2015-03-25 17:05:25 +0800

    Modify

--- Application/Runtime/Cache/Home/565950b2344ef5334c8e8968e08bf32e.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><html>
- <head>
-   <title>Hello World</title>
- </head>
- <body>
-<?php echo $name;?>
- </body>
-</html>
--- Application/Runtime/Cache/Home/b921f456db35b8813ff598dfb13b09fb.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><html>
- <head>
-   <title>Hello World</title>
- </head>
- <body>
-  Hello, <?php echo $content['fname'] . ' ' . $content['lname'];?>
- </body>
-</html>
